f91042ea8b40703717ecb8996a2ee0e9a928e142
Elflanded <shadenitey@Gmail.com>
2020-12-01 14:47:21 +0000

    h

--- python/day1/puzzle.py
+++ python/day1/puzzle.py
@@ -1,7 +1,7 @@
 from itertools import combinations
 from math import prod
 
-with open('input.py', 'r') as f:
+with open('python/day1/input.py', 'r') as f:
     entries = [int(i) for i in f.read().strip().splitlines()]
 
 solution = lambda k: next( prod(comb) for comb in combinations(entries, k) if sum(comb) == 2020 )
